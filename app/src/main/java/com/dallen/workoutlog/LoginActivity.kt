471a40edabd937f45180649302a0fb04d91874e8
-abinsinguza-dallen <dallenabinsinguza@gmail.com>
2022-06-02 19:47:35 +0300

    first commit

---
+++ app/src/main/java/com/dallen/workoutlog/LoginActivity.kt
@@ -0,0 +1,46 @@
+package com.dallen.workoutlog
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import com.google.android.material.textfield.TextInputEditText
+import com.google.android.material.textfield.TextInputLayout
+
+class LoginActivity : AppCompatActivity() {
+    lateinit var btnLogin:Button
+    lateinit var tilEmail:TextInputLayout
+    lateinit var etEmail:TextInputEditText
+    lateinit var tilPassword:TextInputLayout
+    lateinit var etPassword:TextInputEditText
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+        btnLogin=findViewById(R.id.btnLogin)
+        etPassword=findViewById(R.id.etPassword)
+        tilPassword=findViewById(R.id.tilPassword)
+        etEmail=findViewById(R.id.etEmail)
+        tilEmail=findViewById(R.id.tilEmail)
+        btnLogin.setOnClickListener { validateLogin() }
+    }
+    fun validateLogin(){
+        var error=false
+        tilEmail.error=null
+        tilPassword.error=null
+        var email=etEmail.text.toString()
+        if (email.isBlank()){
+            tilEmail.error="Email is required"
+            error=true
+        }
+
+
+        var password=etPassword.text.toString()
+        if (password.isBlank()){
+            tilPassword.error="Password is required"
+            error=true
+        }
+        if (error!=true){
+
+    }
+
+}
+     }
--- app/src/main/java/com/dallen/workoutlog/SlashActivity.kt
+++ app/src/main/java/com/dallen/workoutlog/SlashActivity.kt
@@ -1,11 +1,12 @@
 package com.dallen.workoutlog
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class SlashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        startActivity(Intent(this,LoginActivity::class.java))
     }
 }
